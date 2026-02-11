3822ee726fb8bc7015e2aaf8cd7d995f7340849d
Abhishek <ak6471494@gmail.com>
2026-02-11 16:50:52 +0530

    update project

---
+++ sum.js
@@ -0,0 +1,33 @@
+// function sum ( a,b) {
+//     let sum =a+b;
+//     return sum;
+// }
+// let num = sum(2,3);
+// console.log(sum (44,44));
+// function gretting (name) {
+//     console.log(`Namaste ${name} uncle`);
+   
+// }
+// gretting('abhi');
+// function issOdd (number){
+//     let rem= number % 2==1;
+//     // let number=rem=1;
+//     return `issOdd`;
+// }
+// console.log(issOdd (4.2));
+// let product = {
+//     costomer : 'Abhishek',
+//     age : 20,
+//     laptop:'asus',
+// };
+// console.log(product);
+// product.age=40;
+// console.log(product.age);
+let product ={
+    prize : 80,
+    name :'prince',
+    prizeDeal: function (){
+        console.log(`prize of the product is ${product.prize}`)
+    }
+};
+product.prizeDeal();
